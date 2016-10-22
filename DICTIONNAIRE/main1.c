ab1851a56219382d135df477dc87496e039a9d89
Bérangère La Touche <berangerelatouche@macbook-pro-2.home>
2016-10-22 15:11:43 +0200

    gestion conflits

--- DICTIONNAIRE/main1.c
+++ DICTIONNAIRE/main1.c
@@ -9,22 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-<<<<<<< HEAD:DICTIONNAIRE/main.c
-#include "functions.h"
-
-int main(int argc, const char * argv[]) {
-    
-    char* filename = "souce.txt" ;
-    
-    findWord(filename);
-=======
 #include "gestbib.h"
-
 
 int main(int argc, const char * argv[]) {
     
     menu();
->>>>>>> develop:DICTIONNAIRE/main1.c
     
     return 0;
 }
