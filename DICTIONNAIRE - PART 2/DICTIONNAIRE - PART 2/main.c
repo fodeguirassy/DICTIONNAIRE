e1e87f8a74aca3664178798a7ca1881bc0e0764f
fodeguirassy <fofofofodev@macbook-pro-de-fofo.home>
2016-11-13 20:11:27 +0100

    part 2 project

---
+++ DICTIONNAIRE - PART 2/DICTIONNAIRE - PART 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DICTIONNAIRE - PART 2
+//
+//  Created by fofo fofodev on 13/11/2016.
+//  Copyright © 2016 PROJECT SEMAINE C. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
